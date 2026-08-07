0254a2e19ffb313bf60ab897b92a650cb5deea7c
santirodriguez <15202529+santirodriguez@users.noreply.github.com>
2026-08-07 16:46:19 -0300

    Add spinner selection helper

---
+++ app/src/main/java/com/santiagorodriguez/countaway/ui/SimpleItemSelectedListener.kt
@@ -0,0 +1,14 @@
+package com.santiagorodriguez.countaway.ui
+
+import android.view.View
+import android.widget.AdapterView
+
+class SimpleItemSelectedListener(
+    private val onSelected: (position: Int) -> Unit,
+) : AdapterView.OnItemSelectedListener {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        onSelected(position)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) = Unit
+}
