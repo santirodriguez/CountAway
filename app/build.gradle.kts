16715ff9ec60d626bd2fae8ff8ad6906d770f77c
santirodriguez <15202529+santirodriguez@users.noreply.github.com>
2026-08-07 17:37:22 -0300

    Enable release optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
         }
     }
 
