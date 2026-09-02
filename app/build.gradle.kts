9d4db47b7eebb901e4e6262dd56f0fb52879d12d
Santiago Rodriguez <15202529+santirodriguez@users.noreply.github.com>
2026-09-02 14:37:11 -0300

    Bump CountAway to 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.santiagorodriguez.countaway"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.6"
+        versionCode = 7
+        versionName = "1.1.7"
     }
 
     buildTypes {
